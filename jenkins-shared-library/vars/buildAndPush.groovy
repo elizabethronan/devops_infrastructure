744ef8fd3cb3c94c217323dcf96a22dac6d91837
Elizabeth Ronan <eronan@uchicago.edu>
2026-03-11 19:07:58 -0500

    Fixing credential block

--- jenkins-shared-library/vars/buildAndPush.groovy
+++ jenkins-shared-library/vars/buildAndPush.groovy
@@ -1,7 +1,13 @@
-def call(String imageName, String imageTag, String credentials) {
-    sh "docker build -t ${imageName}:${imageTag} ."
-    sh "docker tag ${imageName}:${imageTag} ${imageName}:latest"
-    sh "echo ${credentials.split(':')[1]} | docker login -u ${credentials.split(':')[0]} --password-stdin"
-    sh "docker push ${imageName}:${imageTag}"
-    sh "docker push ${imageName}:latest"
+def call(String imageName, String imageTag, String credentialsId) {
+    withCredentials([usernamePassword(
+        credentialsId: credentialsId,
+        usernameVariable: 'DOCKER_USER',
+        passwordVariable: 'DOCKER_PASS'
+    )]) {
+        sh '''
+            echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+            docker push ''' + imageName + ':' + imageTag + '''
+            docker push ''' + imageName + ''':latest
+        '''
+    }
 }
