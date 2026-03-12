4d763e4aa262cfced16f3d5395fc654a5f4ce90e
Elizabeth Ronan <eronan@uchicago.edu>
2026-03-11 19:11:17 -0500

    Fixing image tag

--- jenkins-shared-library/vars/buildAndPush.groovy
+++ jenkins-shared-library/vars/buildAndPush.groovy
@@ -1,13 +1,18 @@
 def call(String imageName, String imageTag, String credentialsId) {
+    // Build first
+    sh "docker build -t ${imageName}:${imageTag} ."
+    sh "docker tag ${imageName}:${imageTag} ${imageName}:latest"
+    
+    // Then push with same tag
     withCredentials([usernamePassword(
         credentialsId: credentialsId,
         usernameVariable: 'DOCKER_USER',
         passwordVariable: 'DOCKER_PASS'
     )]) {
-        sh '''
-            echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
-            docker push ''' + imageName + ':' + imageTag + '''
-            docker push ''' + imageName + ''':latest
-        '''
+        sh """
+            echo "\$DOCKER_PASS" | docker login -u "\$DOCKER_USER" --password-stdin
+            docker push ${imageName}:${imageTag}
+            docker push ${imageName}:latest
+        """
     }
 }
