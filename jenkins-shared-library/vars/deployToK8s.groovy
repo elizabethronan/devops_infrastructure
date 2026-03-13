0c39c70c1179548e8e8ea63df42070592512c945
Elizabeth Ronan <eronan@uchicago.edu>
2026-03-12 19:29:31 -0500

    Editing groovy to create deployments

--- jenkins-shared-library/vars/deployToK8s.groovy
+++ jenkins-shared-library/vars/deployToK8s.groovy
@@ -1,4 +1,11 @@
 def call(String serviceName, String imageName, String imageTag, String namespace = 'dev') {
-    sh "kubectl set image deployment/${serviceName} ${serviceName}=${imageName}:${imageTag} -n ${namespace}"
-    sh "kubectl set image deployment/${serviceName} ${serviceName}=${imageName}:${imageTag} -n ${namespace}"
+    sh """
+        kubectl create deployment ${serviceName} \
+            --image=${imageName}:${imageTag} \
+            --namespace=${namespace} \
+            --dry-run=client -o yaml | kubectl apply -f -
+        kubectl set image deployment/${serviceName} \
+            ${serviceName}=${imageName}:${imageTag} \
+            -n ${namespace}
+    """
 }
